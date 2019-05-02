30aae6742f89f2ab89ddcdb3c1e09e01b2d9bac0
DavidR1997 <david.rowsellwork@gmail.com>
2019-05-02 15:53:55 +1000

    the swarm

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -5,31 +5,37 @@ using UnityEngine;
 public class CameraLook : MonoBehaviour
 {
     public Transform attachedCamera;
-    public bool isCursorHiden = true;
+    public bool isCursorHidden = true;
     public float minPitch = -80f, maxPitch = 80f;
     public Vector2 speed = new Vector2(120f, 120f);
 
-    private Vector2 euler;
+    private Vector2 euler; // Current rotation of the camera
+
     // Start is called before the first frame update
     void Start()
     {
-        //is the cursor supposed to be hidden?
-        if (isCursorHiden)
+        // Is the cursor supposed to be hidden?
+        if (isCursorHidden)
         {
+            // Lock and Hide it
             Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
+            Cursor.visible = false; // ... Invisible!
         }
+        // Get current camera euler
         euler = transform.eulerAngles;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        // Rotate the euler with Mouse movement
         euler.y += Input.GetAxis("Mouse X") * speed.x * Time.deltaTime;
         euler.x -= Input.GetAxis("Mouse Y") * speed.y * Time.deltaTime;
 
+        // Clamp the camera on pich
         euler.x = Mathf.Clamp(euler.x, minPitch, maxPitch);
 
+        // Rotate the Player and Transform seperately
         transform.localEulerAngles = new Vector3(0f, euler.y, 0f);
         attachedCamera.localEulerAngles = new Vector3(euler.x, 0f, 0f);
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Header("Player Move Speeds")]
     public float runSpeed = 8f;
     public float walkSpeed = 6f;
     public float gravity = -10f;
     public float jumpHeight = 15f;
     public float groundRayDistance = 1.1f;
 
-    private CharacterController controller; //Reference to Character Controller
-    private Vector3 motion; // is the movement offset per frame
+    private CharacterController controller; // Reference to CharacterController
+    private Vector3 motion; // Is the movement offset per frame
 
-    private void OnDrawGizmos()
+    void OnDrawGizmos()
     {
         Ray groundRay = new Ray(transform.position, -transform.up);
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, transform.position - transform.up * groundRayDistance);
     }
+
+    // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -28,50 +29,52 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get W, A, S, D or Left, Right, Up, Down Input
         float inputH = Input.GetAxis("Horizontal");
         float inputV = Input.GetAxis("Vertical");
-
+        // Move character motion with inputs
         Move(inputH, inputV);
-        //is the player grounded
+        // Is the Player grounded?
         if (IsGrounded())
         {
+            // Cancel gravity
             motion.y = 0f;
-            //if the jump button has been pressed
+            // Pressing Jump Button?
             if (Input.GetButtonDown("Jump"))
             {
-                //player jumps
+                // Make the Player Jump!
                 motion.y = jumpHeight;
             }
         }
-        //Apply Gravity
+        // Apply gravity
         motion.y += gravity * Time.deltaTime;
-        //Move the controller with motion
+        // Move the controller with motion
         controller.Move(motion * Time.deltaTime);
     }
-    //Check if the player is touching the ground
+
+
+    // Check if the player is touching the ground
     bool IsGrounded()
     {
         return controller.isGrounded;
-        ////Raycast below the player
+        //
+        //// Raycast below the player
         //Ray groundRay = new Ray(transform.position, -transform.up);
-
-        ////if hitting something
+        //// If hitting something
         //return Physics.Raycast(groundRay, groundRayDistance);
-
     }
-    //Move characters motion in direction of input
+
+    // Move the character's motion in direction of input
     void Move(float inputH, float inputV)
     {
-        //Generate direction from input
+        // Generate direction from input
         Vector3 direction = new Vector3(inputH, 0f, inputV);
 
-        //Convert local space to world space direction
+        // Convert local space to world space direction
         direction = transform.TransformDirection(direction);
 
-        //Apply motion to only x and z
+        // Apply motion to only X and Z
         motion.x = direction.x * walkSpeed;
         motion.z = direction.z * walkSpeed;
-        
-
     }
 }
