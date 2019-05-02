7fd9d270f1ed4aeb2829226d45c4f8f21bae0443
DavidR1997 <david.rowsellwork@gmail.com>
2019-05-02 14:20:10 +1000

    Initial commit

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public Transform attachedCamera;
+    public bool isCursorHiden = true;
+    public float minPitch = -80f, maxPitch = 80f;
+    public Vector2 speed = new Vector2(120f, 120f);
+
+    private Vector2 euler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //is the cursor supposed to be hidden?
+        if (isCursorHiden)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        euler = transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        euler.y += Input.GetAxis("Mouse X") * speed.x * Time.deltaTime;
+        euler.x -= Input.GetAxis("Mouse Y") * speed.y * Time.deltaTime;
+
+        euler.x = Mathf.Clamp(euler.x, minPitch, maxPitch);
+
+        transform.localEulerAngles = new Vector3(0f, euler.y, 0f);
+        attachedCamera.localEulerAngles = new Vector3(euler.x, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Player Move Speeds")]
+    public float runSpeed = 8f;
+    public float walkSpeed = 6f;
+    public float gravity = -10f;
+    public float jumpHeight = 15f;
+    public float groundRayDistance = 1.1f;
+
+    private CharacterController controller; //Reference to Character Controller
+    private Vector3 motion; // is the movement offset per frame
+
+    private void OnDrawGizmos()
+    {
+        Ray groundRay = new Ray(transform.position, -transform.up);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position - transform.up * groundRayDistance);
+    }
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+
+        Move(inputH, inputV);
+        //is the player grounded
+        if (IsGrounded())
+        {
+            motion.y = 0f;
+            //if the jump button has been pressed
+            if (Input.GetButtonDown("Jump"))
+            {
+                //player jumps
+                motion.y = jumpHeight;
+            }
+        }
+        //Apply Gravity
+        motion.y += gravity * Time.deltaTime;
+        //Move the controller with motion
+        controller.Move(motion * Time.deltaTime);
+    }
+    //Check if the player is touching the ground
+    bool IsGrounded()
+    {
+        return controller.isGrounded;
+        ////Raycast below the player
+        //Ray groundRay = new Ray(transform.position, -transform.up);
+
+        ////if hitting something
+        //return Physics.Raycast(groundRay, groundRayDistance);
+
+    }
+    //Move characters motion in direction of input
+    void Move(float inputH, float inputV)
+    {
+        //Generate direction from input
+        Vector3 direction = new Vector3(inputH, 0f, inputV);
+
+        //Convert local space to world space direction
+        direction = transform.TransformDirection(direction);
+
+        //Apply motion to only x and z
+        motion.x = direction.x * walkSpeed;
+        motion.z = direction.z * walkSpeed;
+        
+
+    }
+}
