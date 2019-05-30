e2157f1b06d80dd80cd63d53c7110f54618aea27
DavidR1997 <david.rowsellwork@gmail.com>
2019-05-30 12:12:33 +1000

    ai

---
+++ Assets/Scripts/AI/SteeringBehaviours/Flee.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+[CreateAssetMenu(fileName = "Flee", menuName = "SteeringBehaviours/Flee", order = 1)]
+public class Flee : SteeringBehaviours
+{
+
+    public float stoppingDistance = 1f;
+    public override void OnDrawGizmosSelected(AI owner)
+    {
+        Gizmos.color = Color.red;
+        float distance = Vector3.Distance(owner.target.position, owner.transform.position);
+        Gizmos.DrawWireSphere(owner.target.position, distance - stoppingDistance);
+    }
+    public override Vector3 GetForce(AI owner)
+    {
+        //Create a value to return later
+        Vector3 force = Vector3.zero;
+        //Modify value here
+        if (owner.hasTarget)
+        {
+            //Get direction from ai agent to target
+            force += owner.transform.position - owner.target.position;
+        }
+        //Return value
+        return force.normalized;
+    }
+}
